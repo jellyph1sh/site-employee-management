e448d477e7673145c2d4e7e68f3507f2a83f537f
jellyph1sh <sacha.sorgiati@gmail.com>
2023-10-17 16:13:54 +0200

    [WSERVER] :construction: | Build an example of api sending data.

--- webserver/index.js
+++ webserver/index.js
@@ -4,7 +4,11 @@ const PORT = 3001
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
-    console.log('Server started !');
+    console.log('Server started ! http://localhost:3001');
     console.log(`Server now listening on ${PORT}`);
 })
