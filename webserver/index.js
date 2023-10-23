22ced3cc8c6e41e8234e7c594ac07276f3d46697
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-10-23 23:02:16 +0000

    [WSERVER] :sparkles: | API Request & Sqlite Request from DB

--- webserver/index.js
+++ webserver/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const Database = require('./Database');
+const cors = require('cors')
 
 const PORT = 3001
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hello from server!" });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Allow all origins for demonstration purposes.
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from server!' });
 });
+
+app.post('/test', (req, res) => {
+    console.log(req.body);
+    res.send("POST Request Called")
+})
+
+app.put('/put', (req, res) => {
+    console.log(req.body);
+    res.send("PUT Request Called")
+})
+
+app.get('/employees', async (req, res) => {
+    let employees = await Database.Read('company.db', 'SELECT * FROM Employees;');
+    res.json(employees);
+})
 
 app.listen(PORT, () => {
     console.log('Server started ! http://localhost:3001');
