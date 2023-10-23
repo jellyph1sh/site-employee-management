3bbf181563b23dd83e0aefd3d9cede70baa8a3a2
ckevin <kevin.chaffaux@ynov.com>
2023-10-23 13:40:19 +0200

    [CSERVER] :iphone: | add responsive for nav bar and title component

--- Client/src/Components/NavBar/navBar.jsx
+++ Client/src/Components/NavBar/navBar.jsx
@@ -9,20 +9,32 @@ export const NavBar = () => {
                 <h1 className='subtitle'>Pro</h1>
                 <span className='dots'></span>
             </div>
+            <div className="container_title_nav_mobile">
+                <span className='dots'></span>
+                <h1 className='main_title'>Toka</h1>
+                <h1 className='subtitle'>Pro</h1>
+                <span className='dots'></span>
+            </div>
             <img className='background_nav_header' src="./src/assets/images/background_nav.png" alt="background_nav_image" />
             <div className='container_categories'>
-                <div className='container_one_cat_nav'>
-                    <span></span>
-                    <a className='link' href="">my employees</a>
-                </div>
-                <div className='container_one_cat_nav'>
-                    <span></span>
-                    <a className='link' href="">my employees</a>
-                </div>
-                <div className='container_one_cat_nav'>
-                    <span></span>
-                    <a className='link' href="">my employees</a>
-                </div>
+                <a href="">
+                    <div className='container_one_cat_nav'>
+                        <span></span>
+                        <p className='link' href="">my employees</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div className='container_one_cat_nav'>
+                        <span></span>
+                        <p className='link' href="">my employees</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div className='container_one_cat_nav'>
+                        <span></span>
+                        <p className='link' href="">my employees</p>
+                    </div>
+                </a>
               
             </div>
             <div className='signIn_container'>
