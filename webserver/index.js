cfd5c53b39dc71636e1b5bad25bfd17266675abb
jellyph1sh <sacha.sorgiati@gmail.com>
2023-10-29 17:37:37 +0100

    [WSERVER] :sparkles: | Add post method to check if user trying to connect exist and is valid.

--- webserver/index.js
+++ webserver/index.js
@@ -26,6 +26,16 @@ app.post('/employee', async (req, res) => {
     res.json({status: true})
 })
 
+app.post('isValidUser', async (req, res) => {
+    const emp = req.body;
+    let user = await Database.Read('company.db', 'SELECT Jobs.permissionLevel FROM Accounts JOIN Employees ON Employees.employeeId = Accounts.employeeId JOIN Jobs ON Jobs.jobId = Employees.jobId WHERE Accounts.mail = ? AND Accounts.password = ?;', emp.email, emp.password);
+    if (user.length == 0) {
+        res.json({exist: false});
+        return;
+    }
+    res.json({permission: user[0].permissionLevel});
+})
+
 app.put('/updateEmployee', async (req, res) => {
     const emp = req.body;
     const err = await Database.Write('company.db', 'UPDATE Employees SET name = ?, firstName = ?, jobId = ?, birthDate = ?, hireDate = ?, salary = ?, mail = ? WHERE employeeId = ?;', emp.lastname, emp.firstname, emp.jobId, emp.birthdate, emp.hiredate, emp.salary, emp.email, emp.idEmployee);
